590c6bea040bcb8f64761e6851e608977cec5962
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-31 20:20:08 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -55,11 +55,10 @@ def segFormCrack(cl, x, y, w, h, cnf, saved_image):
     #st.image(roi, caption="ROI")
     cv2.imwrite("saved_ROI.jpg", roi)
     segform_model = loadSegFormModel()
-    #preds = segform_model.predict("saved_ROI.jpg").save("crack_pred.jpg")
     preds = segform_model.predict("saved_ROI.jpg")
     print("segmentation results are ")
-    print(preds)
-    preds = preds.save("cracked_pred.jpg")
+    print(preds[0]['segmentation_mask'])
+    preds = segform_model.predict("saved_ROI.jpg").save("crack_pred.jpg")
     crck_pred = Image.open('crack_pred.jpg')
     st.image(crck_pred, caption='crack localization')
 	
