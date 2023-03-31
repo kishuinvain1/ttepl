97cc3cb9f2ced1b17effd015f49c9c6d3ff9f466
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-31 22:03:54 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -52,8 +52,7 @@ def stringToImage(base64_string):
 def toRGB(image):
     return np.array(image)
 	
-def segFormCrack(cl, x, y, w, h, cnf, saved_image):
-    bias = 0
+def segFormCrack(cl, x, y, w, h, cnf, saved_image, bias):
 
     print(".....inside segFormCrack......")
     img = cv2.imread(saved_image)
@@ -65,7 +64,7 @@ def segFormCrack(cl, x, y, w, h, cnf, saved_image):
     h = int(h)
     bias = int(bias)
     roi = img[y-h//2+bias:y+h//2-bias, x-w//2+bias:x+w//2-bias, :]
-    #st.image(roi, caption="ROI")
+    st.image(roi, caption="ROI")
     cv2.imwrite("saved_ROI.jpg", roi)
     segform_model = loadSegFormModel()
     preds = segform_model.predict("saved_ROI.jpg")
@@ -140,7 +139,7 @@ def main():
     #model = project.version(1).model
      
     image, svd_img = load_image()
-    #zoomin_bias = st.number_input('Zoomin Bias')
+    zoomin_bias = st.number_input('Zoomin Bias')
     #st.write('Enter the image URL')
     #url = st.text_input('URL', '')
     result = st.button('Predict')
@@ -190,7 +189,7 @@ def main():
             #st.write(cl)
             #st.write(cnf)
             if(cl == "Non-Broken"):
-                sem_seg_res = segFormCrack(cl, x, y, w, h, cnf, "main_image.jpg")
+                sem_seg_res = segFormCrack(cl, x, y, w, h, cnf, "main_image.jpg", zoomin_bias)
 
     elif(result and option == "Zoomed-in"):
         st.write('Calculating results...')
