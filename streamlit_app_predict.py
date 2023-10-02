4ebbb6d3979b15db4e87838ba7a444de8e6d5d53
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-10-02 17:41:15 +0530

    Fontsize Decreased

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -35,76 +35,16 @@ def load_image():
 
 
 	
-def loadSegFormModel():
-    print("...loading...segformer..")
-    rf = Roboflow(api_key="uhDFc9G6MKjrEvbfHt6B")
-    project = rf.workspace().project("fleetguardcrack")
-    #model = project.version(1).model
-    model = project.version(4).model
-    return model
 
-# Take in base64 string and return PIL image
-def stringToImage(base64_string):
-    imgdata = base64.b64decode(base64_string)
-    return Image.open(io.BytesIO(imgdata))
 
 # convert PIL Image to an RGB image( technically a numpy array ) that's compatible with opencv
 def toRGB(image):
     return np.array(image)
 	
-def segFormCrack(cl, x, y, w, h, cnf, saved_image, bias):
 
-    print(".....inside segFormCrack......")
-    img = cv2.imread(saved_image)
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    #print(img.shape)
-    x = int(x)
-    y = int(y)
-    w = int(w)
-    h = int(h)
-    bias = int(bias)
-    roi = img[y-h//2+bias:y+h//2-bias, x-w//2+bias:x+w//2-bias, :]
-    st.image(roi, caption="ROI")
-    cv2.imwrite("saved_ROI.jpg", roi)
-    segform_model = loadSegFormModel()
-    preds = segform_model.predict("saved_ROI.jpg")
-    print("segmentation results are ")
-    seg_mask = preds[0]['segmentation_mask']
-	
-    
-	
-    print(seg_mask)
-    #seg_mask_read = cv2.imread(seg_mask, 0)
-    #cv2.imwrite("seg_mask.jpg", seg_mask_read)
-    #seg_img = Image.open("seg_mask.jpg")
-    im_bytes = base64.b64decode(seg_mask)
-    im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
-    
-    seg_mask_image = cv2.imdecode(im_arr, 0)
-    print(seg_mask_image)
-    nz_cmp = np.sum(seg_mask_image)
-    print("non-zero is......................................")
-    print(nz_cmp)
-    
-        
-
-    #pil_image = stringToImage(seg_mask)
-    #seg_mask_image = toRGB(pil_image)
-    
-    #st.image(seg_mask_image, caption='segmentation mask')
-    
-    
-    if(nz_cmp > 10):
-        preds = segform_model.predict("saved_ROI.jpg").save("crack_pred.jpg")
-        crck_pred = Image.open('crack_pred.jpg')
-        st.image(crck_pred, caption='crack localization')
-    else:
-        st.write("No Crack Detected")
 	
 def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
     #img = Image.open(saved_image)
-    
-
     #img = cv2.imread(saved_image)
     img = cv2.cvtColor(saved_image,cv2.COLOR_BGR2RGB)
     x = int(x)
@@ -114,11 +54,9 @@ def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
     start_pnt = (x-w//2,y-h//2)
     end_pnt = (x+w//2, y+h//2)
     txt_start_pnt = (x-w//2, y-h//2-15)
-    if(cl == "Crack" or cl == "No-Crack"):
-        cl = "Non-Broken"
     
     img = cv2.rectangle(img, start_pnt, end_pnt, (0,255,0), 10)
-    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 10, cv2.LINE_AA)	
+    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5, cv2.LINE_AA)	
     st.image(img, caption='Resulting Image')	
     
 
