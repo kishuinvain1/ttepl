e5741cf7bf3a02b61ef88562937c16e74799e803
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-10-02 17:39:09 +0530

    Updated

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -129,41 +129,17 @@ def predict(model, url):
 	
 	
 def main():
-    st.title('Crack Detection Final')
-    option = st.selectbox('Image Type',('Normal', 'Zoomed-in'))
-	
-   
-    #Model api for fleet-crack trained on 25_03_23	
-    #rf = Roboflow(api_key="SNxIPCnRCYWXUM9lBAp4")
-    #project = rf.workspace().project("fleet-crack-2-wg5qy")
-    #model = project.version(1).model
+    st.title('Tyre Classification')
+    rf = Roboflow(api_key="iYDj3AF1byBTN6qkTYwP")
+    project = rf.workspace().project("tyre-classification")
+    model = project.version(3).model
      
     image, svd_img = load_image()
-    zoomin_bias = st.number_input('Zoomin Bias')
-    #st.write('Enter the image URL')
-    #url = st.text_input('URL', '')
-    result = st.button('Predict')
-    if(result and option == "Normal"):
-        st.write('Calculating results...')
-        #Model api for FleetGuard trained on 27_03_23
-        #rf2 = Roboflow(api_key="uhDFc9G6MKjrEvbfHt6B")
-        #project2 = rf2.workspace().project("fleetguard")
-        #model2 = project2.version(1).model
-	
-        #Model api for FleetGuard trained on 31_03_23
-        #rf3 = Roboflow(api_key="pAMgIogxlcl3zF1xSCsE")
-        #project3 = rf3.workspace().project("fleetguardobjdet")
-        #model3 = project3.version(1).model
-	
-	#Model api for FleetGuard trained on 31_03_23 with 2 classes (broken/non-broken)
-        rf4 = Roboflow(api_key="q3ZrI4IarL2A3pHuOrt2")
-        project4 = rf4.workspace().project("fleetguardobjdet3")
-        model4 = project4.version(4).model
-	
-	
-	
-	
-        results = predict(model4, svd_img)
+
+    result = st.button('Detect')
+    if result:
+        st.write('Calculating results...')	
+        results = predict(model, svd_img)
         #results = predict(model2, url)
         print("Prediction Results are...")	
         print(results)
@@ -171,7 +147,6 @@ def main():
             st.image(svd_img)
             st.write("No object is detected")
         else:
-		
             new_img_pth = results['predictions'][0]['image_path']
             x = results['predictions'][0]['x']
             y = results['predictions'][0]['y']
@@ -179,67 +154,8 @@ def main():
             h = results['predictions'][0]['height']
             cl = results['predictions'][0]['class']
             cnf = results['predictions'][0]['confidence']
-            if(cl == "Broken" and cnf < 0.8):
-                cl = "Non-Broken"
-            print("printing saved image")
-            #print(svd_img.name)
-	
-            #st.image(svd_img, "saved image")
             drawBoundingBox(svd_img,x, y, w, h, cl, cnf)
-            #st.write(cl)
-            #st.write(cnf)
-            if(cl == "Non-Broken"):
-                sem_seg_res = segFormCrack(cl, x, y, w, h, cnf, "main_image.jpg", zoomin_bias)
-
-    elif(result and option == "Zoomed-in"):
-        st.write('Calculating results...')
-        segform_model = loadSegFormModel()
-        preds = segform_model.predict("main_image.jpg")
-        seg_mask = preds[0]['segmentation_mask']
-	
-    
-	
-        print(seg_mask)
-        #seg_mask_read = cv2.imread(seg_mask, 0)
-        #cv2.imwrite("seg_mask.jpg", seg_mask_read)
-        #seg_img = Image.open("seg_mask.jpg")
-        im_bytes = base64.b64decode(seg_mask)
-        im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
-    
-        seg_mask_image = cv2.imdecode(im_arr, 0)
-        #print(seg_mask_image)
-        nz_cmp = np.sum(seg_mask_image)
-        print("non-zero is......................................")
-        print(nz_cmp)
-        if(nz_cmp > 10):
-		
-            preds = segform_model.predict("main_image.jpg").save("crack_pred.jpg")
-            crck_pred = Image.open('crack_pred.jpg')
-            st.image(crck_pred, caption='crack localization')
-        else:
-            st.write("No Cracks Detected")
-        
-  	
-                  
-        
-       
-        #st.image(res_img, caption='Resulting Image')
-        
-	#results = predict(model, path)
-        #cls_res = results["predictions"][0]["predictions"][0]["class"]
-        #conf_res = results["predictions"][0]["predictions"][0]["confidence"]
-        #if(cls_res == "Crack" and conf_res <= 0.57):
-        #    cls_res = "No-Crack"
-
-        #st.write(cls_res)
-        #st.write(conf_res)
-        #print(results["predictions"][0]["predictions"][0]["class"])
-        #print(results["predictions"][0]["predictions"][0]["confidence"])
-
-
-
-    
-    
+           
 
 if __name__ == '__main__':
     main()
