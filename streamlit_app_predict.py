ac365b9d0cd6711e1a37cb863bb09e1de01806ea
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-10-03 13:15:20 +0530

    change

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -62,7 +62,7 @@ def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
         cl = 'front_wheel-front_disc-5_innerclip_bolts'       
     
     img = cv2.rectangle(img, start_pnt, end_pnt, (0,255,0), 10)
-    img = cv2.putText(img, "KTM200-"+cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 140, 0), 2, cv2.LINE_AA)	
+    img = cv2.putText(img, "KTM200-"+cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (165, 255, 0), 2, cv2.LINE_AA)	
     st.image(img, caption='Resulting Image')	
     
 
