1bf1856871cb7efbedc2e8932292eed416c0fbb5
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-11-29 17:17:06 +0530

    ...

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -53,11 +53,11 @@ def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
     h = int(h)
     start_pnt = (x-w//2,y-h//2)
     end_pnt = (x+w//2, y+h//2)
-    txt_start_pnt = (x-w//2, y-h//2-15)
+    txt_start_pnt = (x-w//2-20, y-h//2)
        
     
     img = cv2.rectangle(img, start_pnt, end_pnt, (0,255,0), 10)
-    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 5, cv2.LINE_AA)	
+    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 5, cv2.LINE_AA)	
     #st.image(img, caption='Resulting Image')	
 
     return img
