470250b8565e197bd7908c673bdf69a38702aeaf
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-11-29 17:12:35 +0530

    ...

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -54,15 +54,10 @@ def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
     start_pnt = (x-w//2,y-h//2)
     end_pnt = (x+w//2, y+h//2)
     txt_start_pnt = (x-w//2, y-h//2-15)
-    if cl == 'front-alloy-outer':
-        cl = 'front_wheel-front_disc-6_innerclip_bolts'
-    elif cl == 'rear-spokes-inner':
-        cl = 'rear_wheel-rear_disc-5_innerclip_bolts' 
-    elif cl == 'front-alloy-inner':
-        cl = 'front_wheel-front_disc-5_innerclip_bolts'       
+       
     
     img = cv2.rectangle(img, start_pnt, end_pnt, (0,255,0), 10)
-    img = cv2.putText(img, "KTM200-"+cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 1, cv2.LINE_AA)	
+    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)	
     #st.image(img, caption='Resulting Image')	
 
     return img
